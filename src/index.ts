f0e27c131ea400b97ecef80b4a767549e99505db
Klevisonf <silvaklevison11@gmail.com>
2026-02-25 01:47:43 -0300

    chore: add zod and fastify-type-provider-zod setup

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,50 @@
 import "dotenv/config";
 import Fastify from "fastify";
-const fastify = Fastify({
+import {
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { z } from "zod";
+
+const app = Fastify({
   logger: true,
 });
 
-fastify.get("/", async function handler() {
+// Rota simples
+app.get("/", async function handler() {
   return { hello: "world" };
 });
 
+// Rota com type provider + schema Zod
+app.withTypeProvider<ZodTypeProvider>().route({
+  method: "GET",
+  url: "/hello",
+  schema: {
+    description: "Hello world",
+    tags: ["hello-world"],
+    response: {
+      200: z.object({
+        message: z.string(),
+      }),
+    },
+  },
+  handler: () => {
+    return {
+      message: "Hello world",
+    };
+  },
+});
+
+// Registrando os compilers
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+// Start do servidor
 try {
-  await fastify.listen({ port: Number(process.env.PORT) });
+  await app.listen({ port: Number(process.env.PORT) });
+  console.log(`🚀 Server running at http://localhost:${process.env.PORT}`);
 } catch (err) {
-  fastify.log.error(err);
+  app.log.error(err);
   process.exit(1);
 }
